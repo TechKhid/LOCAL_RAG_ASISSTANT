8d5afd6596eb621f5aedcf9e32c6f3983cbb17cc
TechKhid <techkhid233@gmail.com>
2026-01-24 04:06:48 +0000

    added uitility function to index text chunks

--- opensearch_vector_index_setup.py
+++ opensearch_vector_index_setup.py
@@ -10,25 +10,42 @@ client = OpenSearch(
     verify_certs=False
 )
 
-index_name = "pdf-rag"
+# index_name = "pdf-rag"
+# config = {
+#             "settings":{
+#                 "index":{
+#                     "knn":True
+#                 }
+#             },
+#             "mappings":{
+#                 "properties": {
+#                     "text": {
+#                         "type": "knn_vector",
+#                         "dimension": 384
+#                     },
+#                     "source": {"type": "keyword"}
+#                 }
+#             }
+#         }
 
 
-if not client.indices.exists(index=index_name):
-    client.indices.create(index=index_name,
-                          body={
-                              "settings":{
-                                  "index":{
-                                      "knn":True
-                                  }
-                              },
-                              "mappings":{
-                                  "properties": {
-                                      "text": {
-                                          "type": "knn_vector",
-                                          "dimension": 384
-                                      },
-                                      "source": {"type": "keyword"}
-                                  }
-                              }
-                          }
-                        )
+
+def create_vector_index(index_name, config):
+    if not client.indices.exists(index=index_name):
+        client.indices.create(index=index_name,
+                            body= config
+                            )
+
+
+
+def index_chunks(chunks, embedding, source, index_name):
+    for i, (text, embed) in enumerate(zip(chunks, embedding)):
+        client.index(
+            index=index_name,
+            id=i,
+            body={
+                "text": text,
+                "embedding": embed,
+                "source": source
+            }
+        )
